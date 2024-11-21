64e8165e6158f6e048122409905e288110a8f6a0
VSjnk <100817326+VSjnk@users.noreply.github.com>
2024-11-21 09:25:49 -0800

    Added Sprinting
    Sprinting is now a thing

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 50
+var SPEED = 150
+var stamina = 100
 
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("Left","Right"),Input.get_axis("Up","Down"))
@@ -10,8 +11,24 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
+	
+	#Recharges your stamina
+	print(stamina)
+	if !Input.is_action_pressed("Sprint"):
+		if stamina < 100:
+			stamina += 0.5
+			
+	#Handels your sprinting
+	if Input.is_action_pressed("Sprint") and stamina > 0:
+		SPEED = 250
+		#Looses Stamina over time
+		stamina -= 1
+	else:
+		SPEED = 150
 
 func _input(event):
 	if event is InputEventKey:
+		
+		
 		
 		pass
