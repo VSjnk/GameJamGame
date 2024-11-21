f0b39d4863bfbfe37028edff6e7c8bb65bc49499
VSjnk <100817326+VSjnk@users.noreply.github.com>
2024-11-21 10:10:14 -0800

    Added Player Rotation

--- Player/player.gd
+++ Player/player.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 var SPEED = 150
 var stamina = 100
 
+@onready var animated_sprites = $AnimatedSprites
+
 func _physics_process(delta):
+	#Handels basic movement
 	var direction = Vector2(Input.get_axis("Left","Right"),Input.get_axis("Up","Down"))
 	if direction:
 		velocity = direction * SPEED
@@ -13,7 +16,6 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	#Recharges your stamina
-	print(stamina)
 	if !Input.is_action_pressed("Sprint"):
 		if stamina < 100:
 			stamina += 0.5
@@ -28,5 +30,32 @@ func _physics_process(delta):
 
 func _input(event):
 	if event is InputEventKey:
+		var direction = Vector2(Input.get_axis("Left","Right"),Input.get_axis("Up","Down"))
+		if direction != Vector2(0,0):
+			for i in animated_sprites.get_children():
+				i.hide()
+		var Xaxis = int(direction.x)
+		var Yaxis = int(direction.y)
+		match Yaxis:
+			#up
+			-1:
+				animated_sprites.find_child("Up").show()
+				animated_sprites.find_child("Down").hide()
+				
+			#down
+			1:
+				animated_sprites.find_child("Down").show()
+				animated_sprites.find_child("Up").hide()
+		match Xaxis:
+			#left
+			-1:
+				animated_sprites.find_child("Left").show()
+				animated_sprites.find_child("Right").hide()
+				pass
+			#right
+			1:
+				animated_sprites.find_child("Right").show()
+				animated_sprites.find_child("Left").hide()
+				
 		#Use for actions!
 		pass
