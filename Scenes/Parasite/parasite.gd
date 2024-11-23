f00fcfd11f67a23f229a7b2773f11d8ea3db0815
ifingffd <isaacfleishauer@gmail.com>
2024-11-22 18:27:21 -0800

    pushing random meds

--- Scenes/Parasite/parasite.gd
+++ Scenes/Parasite/parasite.gd
@@ -6,18 +6,17 @@ extends CharacterBody2D
 @onready var random_recalculate_path = $RandomRecalculatePath
 @onready var bodies = $Radius.get_overlapping_bodies()
 @onready var constant_attack_time = $ConstantAttackTime
-
+@onready var meds = preload("res://Scenes/Medicine/medicine.tscn")
+@onready var randommeds = randi_range(1,5)
 var errorOverride = 0
 
 var direction : Vector2
-
 var health = 50
 var speed = 150
 var _delta = 0
 const accel = 10
 const damage = 10
-
-
+var medint = 0
 
 var active = false
 #Finds the Player node as soon as it loads into the scene.
@@ -49,7 +48,7 @@ func _physics_process(delta):
 			if not global_position.is_equal_approx(lookPos):
 				look_at(lookPos)
 		move_and_slide()
-
+	print(randommeds)
 #Decides if the parasite should attack.
 func _on_visible_on_screen_notifier_2d_screen_entered():
 	active = true
@@ -72,4 +71,9 @@ func _on_constant_attack_time_timeout():
 func hurt(damage):
 	health -= damage
 	if health <= 0:
+		if randommeds == 2:
+			medint = meds.instantiate()
+			$"../../Medicine".add_child(medint)
+			medint.global_position = $".".global_position
 		queue_free()
+
